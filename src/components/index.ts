2a91541ff6aeb090592c4f92dbb867dd9d853373
Shola Ayeni <ayenisholah@yahoo.com>
2023-01-25 16:31:45 +0100

    chore: export component

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+export { default as Sidebar } from "./Sidebar"
+export { default as Navbar } from "./Navbar"
+export { default as CustomButton } from "./CustomButton"
+export { default as UploadImagesForm } from "./UploadImagesForm"
+export { default as UploadMetadataForm } from "./UploadMetadataForm"
+export { default as FormField } from "./FormField"
+export { default as DisplayCollections } from "./DisplayCollections"
+export { default as CollectionCard } from "./CollectionCard"
+export { default as NftCards } from "./NftCards"
+
