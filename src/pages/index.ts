1b6fe22c90015c538f82d07e7e766c659eef316d
Shola Ayeni <ayenisholah@yahoo.com>
2023-01-25 11:53:28 +0100

    export pages

---
+++ src/pages/index.ts
@@ -0,0 +1,9 @@
+export { default as Home } from "./Home";
+export { default as Profile } from "./Profile";
+export { default as CreateCollections } from "./CreateCollections.jsx";
+export { default as MintPage } from "./MintPage"
+export { default as UpdateMintPage } from "./UpdateMintPage"
+export { default as UpdateContractPage } from "./UpdateContractPage"
+export { default as UpdateWhitelist } from "./UpdateWhitelist"
+
+
