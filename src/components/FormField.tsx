aafb9607b38b56ce6f9c05b77313d01ce96e4b48
Shola Ayeni <ayenisholah@yahoo.com>
2023-01-25 11:33:47 +0100

    feat: form field component

---
+++ src/components/FormField.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+const FormField = ({
+  labelName,
+  placeholder,
+  inputType,
+  value,
+  handleChange,
+  isTextArea,
+  style,
+  disabled = false,
+}: IFormField) => {
+  return (
+    <label className="flex-1 w-full flex flex-col">
+      {labelName && (
+        <span
+          className={`font-epilogue font-medium text-[14px] leading-[22px] text-[#808191] mb-[10px] ${style}`}
+        >
+          {labelName}
+        </span>
+      )}
+      {/* <input
+                required
+                value={value}
+                onChange={handleChange}
+                type={inputType}
+                step="0.1"
+                placeholder={placeholder}
+                className="py-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px]"
+            /> */}
+
+      {isTextArea ? (
+        <textarea
+          required
+          value={value}
+          onChange={handleChange}
+          rows={10}
+          placeholder={placeholder}
+          className={`py-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px] mb-8 ${style}`}
+        />
+      ) : (
+        <input
+          disabled={disabled}
+          required
+          value={value}
+          onChange={handleChange}
+          type={inputType}
+          step="0.1"
+          placeholder={placeholder}
+          className={`py-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px] mb-8 ${style}`}
+        />
+      )}
+    </label>
+  );
+};
+
+export default FormField;
+
+interface IFormField {
+  labelName?: string;
+  placeholder?: string;
+  value: string | number;
+  inputType?: string;
+  handleChange?: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  isTextArea?: boolean;
+  style?: string;
+  disabled?: boolean;
+}
